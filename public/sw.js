51fc2f2eb7149cb507ee6588a89a7ccdb6a0acc4
Dennis Wanjiku <131625108+mwangiiii@users.noreply.github.com>
2025-09-27 21:57:05 +0300

    fix: deleting the sw.js file to test whether it causes the product/id: route to fail

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+// Add a clear cache function to the service worker
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'CLEAR_CACHE') {
+    caches.keys().then((cacheNames) => {
+      cacheNames.forEach((cacheName) => {
+        caches.delete(cacheName);
+      });
+    });
+  }
+});
