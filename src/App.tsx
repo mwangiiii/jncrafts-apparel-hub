04de39771047512f733b87da9434b550031f9920
Dennis Wanjiku <131625108+mwangiiii@users.noreply.github.com>
2025-10-02 17:03:38 +0300

    fix: corrected the display of the paystack Redirect view

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import AuthConfirm from "./pages/AuthConfirm";
@@ -19,29 +19,31 @@ import SizeChart from "./pages/SizeChart";
 import PaymentSuccessPage from "./pages/PaystackRedirect";
 
 const App = () => (
-  <TooltipProvider>
-    <Toaster />
-    <Sonner />
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/auth" element={<Auth />} />
-      <Route path="/auth/confirm" element={<AuthConfirm />} />
-      <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/admin/order/:orderId" element={<AdminOrderDetail />} />
-      <Route path="/admin/products" element={<AdminProducts />} />
-      <Route path="/admin/categories" element={<AdminCategories />} />
-      <Route path="/admin/content" element={<AdminContentManager />} />
-      <Route path="/admin/messages" element={<AdminMessages />} />
-      <Route path="/messages" element={<UserMessages />} />
-      <Route path="/messages/:conversationId" element={<UserMessages />} />
-      <Route path="/wishlist" element={<Wishlist />} />
-      <Route path="/product/:id" element={<ProductDetail />} />
-      <Route path="/sizechart" element={<SizeChart />} />
-      s
-      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </TooltipProvider>
+  <BrowserRouter>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/auth/confirm" element={<AuthConfirm />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/order/:orderId" element={<AdminOrderDetail />} />
+        <Route path="/admin/products" element={<AdminProducts />} />
+        <Route path="/admin/categories" element={<AdminCategories />} />
+        <Route path="/admin/content" element={<AdminContentManager />} />
+        <Route path="/admin/messages" element={<AdminMessages />} />
+        <Route path="/messages" element={<UserMessages />} />
+        <Route path="/messages/:conversationId" element={<UserMessages />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/sizechart" element={<SizeChart />} />
+        <Route path="/paystack-redirect" element={<PaymentSuccessPage />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </TooltipProvider>
+  </BrowserRouter>
 );
 
 export default App;
